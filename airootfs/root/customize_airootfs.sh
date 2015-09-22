852b2aa5e4ab02daaf7192aeabbe56e5b48785cd
zelbrium <zelbrium@gmail.com>
2015-09-22 15:39:36 -0400

    Added more customizations and packages

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -7,15 +7,20 @@ locale-gen
 
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
+# Compile GTK schemas
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+# Default stuff
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 chmod 700 /root
 
+# Create live user
 groupadd -r autologin
-! id arch  && useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel,autologin" -s /bin/bash veltlive
-#echo "veltlive:velt" | chpasswd
-#echo "veltlive ALL=(ALL:ALL) ALL" >> /etc/sudoers
+useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel,autologin" -s /bin/bash veltlive
+echo "veltlive ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
+# Default stuff
 sed -i 's/#\(PermitRootLogin \)no/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
